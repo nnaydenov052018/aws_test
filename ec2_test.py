15371036476ec5264ec8ad97620cd5eceb55b75f
Nikola Naydenov <kolio4043@abv.bg>
2020-04-19 13:58:34 +0300

    load aws creds from json file

--- ec2_test.py
+++ ec2_test.py
@@ -1,10 +1,18 @@
 import logging
+import json
 import boto3
 from botocore.exceptions import ClientError
 
 class AwsEc2(object):
 
-    ec2_client = boto3.client('ec2')
+    def __init__(self, aws_access_key_id, aws_secret_access_key, aws_region):
+        self.aws_access_key_id = aws_access_key_id
+        self.aws_secret_access_key = aws_secret_access_key
+        self.aws_region = aws_region
+        self.ec2_client = boto3.client('ec2', 
+                                        aws_access_key_id = self.aws_access_key_id,
+                                        aws_secret_access_key = self.aws_secret_access_key,
+                                        region_name=self.aws_region)
 
     def create_ec2_instance(self, image_id=None, instance_type=None, keypair_name=None, mincount=None, maxcount=None):
         # Provision and launch the EC2 instance
@@ -65,14 +73,17 @@ class AwsEc2(object):
 
 
 ## Create class instance
-ec2 = AwsEc2()
+with open('.config\\.config.json') as f:
+  aws_creds = json.load(f)
+ec2 = AwsEc2(aws_creds['access_key_id'], aws_creds['secret_access_key'], aws_creds['aws_region'])
+
 ## Describe instances
-# response = ec2.describe_ec2_instances()
-# print(response)
+response = ec2.describe_ec2_instances()
+print(response)
 
 ## Terminate instances
-terminate = ec2.terminate_ec2_instances(['i-0804ba3592cc35489'])
-print(terminate)
+# terminate = ec2.terminate_ec2_instances(['i-0804ba3592cc35489'])
+# print(terminate)
 
 ## Create instances
 # instance_info = ec2.create_ec2_instance('ami-076431be05aaf8080', 't2.micro', 'test-nnaydenov', 2, 4)
